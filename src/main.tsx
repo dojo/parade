ec15d8826e1c723e346940b6cb0ce2b6a306d3ff
Matt Gadd <mgadd@sitepen.com>
2019-11-26 14:48:05 +0000

    inject themes

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,11 @@ import renderer, { tsx } from '@dojo/framework/core/vdom';
 import Registry from '@dojo/framework/core/Registry';
 import { registerThemeInjector } from '@dojo/framework/core/mixins/Themed';
 import { registerRouterInjector } from '@dojo/framework/routing/RouterInjector';
-import dojo from '@dojo/themes/dojo';
-import '@dojo/themes/dojo/index.css';
 
 import routes from './routes';
 import App from './App';
 
-export default ({ configs, themes, domNode, tests }: { configs: any, themes?: any, domNode: HTMLElement, tests: any }) => {
+export default ({ configs, themes = [], domNode, tests }: { configs: any, themes?: any[], domNode: HTMLElement, tests: any }) => {
 	if (global.intern) {
 		const url = new URL(window.location.href);
 		const params = url.searchParams;
@@ -26,7 +24,9 @@ export default ({ configs, themes, domNode, tests }: { configs: any, themes?: an
 			has('docs') === 'false' ? false : has('docs') === 'true' ? true : has('docs')
 		);
 		const registry = new Registry();
-		registerThemeInjector(dojo, registry);
+		themes.map((theme) => {
+			registerThemeInjector(theme, registry);
+		});
 		registerRouterInjector(routes, registry);
 
 		const r = renderer(() => <App includeDocs={includeDocs} configs={configs} />);
