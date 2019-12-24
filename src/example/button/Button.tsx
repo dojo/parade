15d50b2fb3a4a6bca029c33f670ebedc6c2cdc82
Bradley Maier <brdlmaier49@gmail.com>
2019-12-24 03:32:29 -0800

    Fix property parsing (#27)

--- src/example/button/Button.tsx
+++ src/example/button/Button.tsx
@@ -3,12 +3,26 @@ import theme from '@dojo/framework/core/middleware/theme';
 
 import * as css from '../theme/default/button.m.css';
 
-export interface ButtonProperties {
+export interface Base {
+	/** A made up string property */
+	foo: string;
+	/** An optional method that does something and returns a string */
+	baseMethod?(): string;
+	baz: string;
+}
+
+export interface OtherBase {
+	/** An optional base property */
+	otherBaseProp?: string;
+	otherBaseMethod(): string;
+}
+
+type PickSome = Pick<Base, Exclude<keyof Base, 'foo'>>;
+export interface ButtonProperties extends PickSome, OtherBase {
 	label: string;
 }
 
 const factory = create({ theme }).properties<ButtonProperties>();
-
 export default factory(function Button({ properties, middleware: { theme } }) {
 	const { label } = properties();
 	const themedCss = theme.classes(css);
