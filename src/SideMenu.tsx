e5225d10c14b0f368b05614171e63b6dc5780dda
James Milner <jamesmilner22@hotmail.com>
2020-01-13 13:42:08 +0000

    Removes hr element when no examples are available (#38)

--- src/SideMenu.tsx
+++ src/SideMenu.tsx
@@ -47,27 +47,29 @@ export default factory(function SideBar({ properties, middleware: { theme } }) {
 						</li>
 					)}
 				</ul>
-				<hr classes="my-1 border-b-2 border-gray-200" />
-				<ul classes="mt-4 overflow-x-hidden">
-					{(config.widgets[widgetName].examples || []).map((example: any) => {
-						return (
-							<li classes="mb-2">
-								<ActiveLink
-									key={example.filename}
-									classes="block transition-fast hover:translate-r-2px hover:text-gray-900 text-gray-600 font-medium"
-									to="example"
-									params={{
-										widget: widgetName,
-										example: example.filename.toLowerCase()
-									}}
-									activeClasses={['font-bold']}
-								>
-									{example.filename.replace(/([A-Z])/g, ' $1').trim()}
-								</ActiveLink>
-							</li>
-						);
-					})}
-				</ul>
+				{config.widgets[widgetName].examples && <virtual>
+					<hr classes="my-1 border-b-2 border-gray-200" />
+					<ul classes="mt-4 overflow-x-hidden">
+						{(config.widgets[widgetName].examples || []).map((example: any) => {
+							return (
+								<li classes="mb-2">
+									<ActiveLink
+										key={example.filename}
+										classes="block transition-fast hover:translate-r-2px hover:text-gray-900 text-gray-600 font-medium"
+										to="example"
+										params={{
+											widget: widgetName,
+											example: example.filename.toLowerCase()
+										}}
+										activeClasses={['font-bold']}
+									>
+										{example.filename.replace(/([A-Z])/g, ' $1').trim()}
+									</ActiveLink>
+								</li>
+							);
+						})}
+					</ul>
+				</virtual>}
 				<hr classes="my-1 border-b-2 border-gray-200" />
 				<ul classes="mt-4 overflow-x-hidden">
 					<li classes="mb-2">
