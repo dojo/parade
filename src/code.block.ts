6cda9c86d646eaa4dfd92a7be965b3a371587935
Anthony Gubler <agubler@sitepen.com>
2020-03-20 16:59:06 +0000

    fix prism usage for codesandbox

--- src/code.block.ts
+++ src/code.block.ts
@@ -1,10 +1,13 @@
 import * as fs from 'fs';
 import * as path from 'canonical-path';
 const Prism = require('prismjs');
-const loadLanguages = require('prismjs/components/');
+require('prismjs/components/prism-clike');
+require('prismjs/components/prism-javascript');
+require('prismjs/components/prism-jsx');
+require('prismjs/components/prism-typescript');
+require('prismjs/components/prism-tsx');
 
 export default function(exampleFilenames: any) {
-	loadLanguages(['tsx']);
 	const examples: any = {};
 	exampleFilenames.forEach((exampleFilename: string) => {
 		const ts = path.join(process.cwd(), exampleFilename);
